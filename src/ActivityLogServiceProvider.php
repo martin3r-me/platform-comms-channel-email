fe008187fd2bf29f68daf142771aa3da6225e69e
Martin Erren <martin3r@Mac.fritz.box>
2025-07-09 11:33:30 +0200

    livewire component

--- src/ActivityLogServiceProvider.php
+++ src/ActivityLogServiceProvider.php
@@ -3,17 +3,29 @@
 namespace Martin3r\LaravelActivityLog;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class ActivityLogServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // (optional später) Migrations aus dem Package laden
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // Views aus eurem Package laden & publishen
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-activity-log');
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-activity-log'),
+        ], 'views');
+
+        // Livewire-Komponente im Unterordner Activities registrieren
+        if (class_exists(Livewire::class)) {
+            Livewire::component(
+                'activities-index',
+                \Martin3r\LaravelActivityLog\Http\Livewire\Activities\Index::class
+            );
+        }
     }
 
     public function register()
     {
-        // Hier kannst du später Configs, Bindings etc. registrieren
+        // Hier könnt ihr später Configs, Bindings etc. registrieren
     }
 }
